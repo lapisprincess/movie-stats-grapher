68c95990d027bd483999f3a70b19fab3a8a33bfc
TenMileJones <tenmilejones@gmail.com>
2023-12-01 15:49:57 -0800

    Update IMDB_schema_definition.sql
    Finished basic schema; still working on correctly defining foreign keys

--- IMDB_schema_definition.sql
+++ IMDB_schema_definition.sql
@@ -32,5 +32,43 @@ CREATE TABLE Title_Basics (
 	endYear INTEGER -- (YYYY) TV Series end year. ‘\N’ for all other title types
 	runtimeMinutes INTEGER -- primary runtime of the title, in minutes
 	-- OMITTED: genres (string array)
+	FOREIGN KEY(tconst) REFERENCES Akas(titleId)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 );
 
+-- OMITTED: TABLE Crew. 3 attributes, one is tconst, other two are arrays.
+
+CREATE TABLE Episode (
+	tconst TEXT PRIMARY KEY -- alphanumeric identifier of episode
+	parentTconst TEXT -- alphanumeric identifier of the parent TV Series
+	seasonNumber INTEGER -- season number the episode belongs to
+	episodeNumber INTEGER -- episode number of the tconst in the TV series
+	FOREIGN KEY(tconst) REFERENCES Akas(titleId)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+	
+)
+
+CREATE TABLE Principals (
+	tconst TEXT PRIMARY KEY -- alphanumeric unique identifier of the title
+	ordering INTEGER -- a number to uniquely identify rows for a given titleId
+	nconst TEXT -- alphanumeric unique identifier of the name/person
+	category TEXT -- the category of job that person was in
+	job TEXT -- the specific job title if applicable, else '\N'
+	characters TEXT -- the name of the character played if applicable, else '\N'
+)
+
+CREATE TABLE Ratings (
+	tconst TEXT PRIMARY KEY -- alphanumeric unique identifier of the title
+	averageRating REAL -- weighted average of all the individual user ratings
+	numVotes INTEGER -- number of votes the title has received
+)
+
+CREATE TABLE Basics (
+	nconst TEXT PRIMARY KEY -- alphanumeric unique identifier of the name/person
+	primaryName TEXT -- name by which the person is most often credited
+	birthYear INTEGER -- in YYYY format
+	deathYear INTEGER -- in YYYY format if applicable, else '\N'
+	-- OMITTED: primaryProfession (array of strings)
+	-- OMITTED: knownForTitles (array of tconsts)
