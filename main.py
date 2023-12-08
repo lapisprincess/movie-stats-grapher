7b015263499254a18001c52bb331713c5c55e231
Matilda Lerner <lernertilda@gmail.com>
2023-12-08 11:57:21 -0800

    put stuff up
    working on the UI, and shared my homework 3 for reference

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import downloader
 import gzip
 
-prompt = input("Update file? ")
-if 'y' in prompt: downloader.download()
-print(gzip.open("data/name.basics.tsv.gz").read())
+def main():
+    print("Welcome, fellow film buff!")
+
+    datafiles = {}
+    for line in open("datafiles.csv", 'r').readlines():
+        info = []
+        for word in line.split(', '): info.append(word)
+        datafiles.update({info[1]: (info[2], info[1])})
+
+
+
+
+
+
+
+
+
+main()
